ca69f09a091e189c909c93856e325b17b26a6100
Curraje <rajecsingh0@gmail.com>
2020-12-23 04:09:48 -0500

    test bot login

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+import * as Discord from 'discord.js';
+
+
+import * as configProd from "./../bot.prod.json";
+
+const client = new Discord.Client();
+
+client.login(configProd.token);
+
+client.once('ready', () => {
+    console.log("CBT is Online!");
+});
